fc7830fdcc7a076e0098cea82728cca994c06635
dark7py <matveym20022002@gmail.com>
2022-03-17 01:57:31 +0500

    3.1.2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import FilterView from './view/filter-view';
 import SortView from './view/sort-view';
 import PointView from './view/point-view';
 import OfferFormView from './view/offer-form-view';
+import PointListView from './view/point-list-view';
 
 import {render, renderPosition} from './render.js';
 import { generatePoint } from './mock/point';
@@ -18,13 +19,39 @@ const siteMenuElement = tripBody.querySelector('.trip-controls__navigation');
 const filtersElement = tripBody.querySelector('.trip-controls__filters');
 const tripEventsElem = tripBody.querySelector('.trip-events');
 
+const renderPoint = (pointListElement, point) => {
+  const pointComponent = new PointView(point);
+  const pointEditComponent = new OfferFormView(point);
+
+  const replacePointToForm = () => {
+    pointListElement.replaceChild(pointEditComponent.element, pointComponent.element);
+  };
+
+  const replaceFormToPoint = () => {
+    pointListElement.replaceChild(pointComponent.element, pointEditComponent.element);
+  };
+
+  pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replacePointToForm();
+  });
+
+  pointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    replaceFormToPoint();
+  });
+
+  render(pointListElement, pointComponent.element, renderPosition.BEFOREEND);
+};
+
+const pointListComponent = new PointListView();
+render(tripEventsElem, pointListComponent.element, renderPosition.BEFOREEND);
 
 render(headerMenu, new HeaderInfoView(points[0]).element, renderPosition.AFTERBEGIN);
 render(siteMenuElement, new SiteMenuView().element, renderPosition.BEFOREEND);
 render(filtersElement, new FilterView().element, renderPosition.BEFOREEND);
-render(tripEventsElem, new SortView().element, renderPosition.BEFOREEND);
+render(tripEventsElem, new SortView().element, renderPosition.AFTERBEGIN);
 
-render(tripEventsElem, new OfferFormView(points[0]).element, renderPosition.BEFOREEND);
-for (let i = 1; i < POINT_COUNT; i++) {
-  render(tripEventsElem, new PointView(points[i]).element, renderPosition.BEFOREEND);
+
+for (let i = 0; i < POINT_COUNT; i++) {
+  renderPoint(pointListComponent.element, points[i]);
 }
