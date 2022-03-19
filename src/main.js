4185b46054d9a9612c1abb1f33366967a8ea1f63
dark7py <matveym20022002@gmail.com>
2022-03-19 17:08:09 +0500

    3.2
    добавил функциональность кнопке закрытия формы;  страницу, когда нет точек маршрута

--- src/main.js
+++ src/main.js
@@ -5,19 +5,20 @@ import SortView from './view/sort-view';
 import PointView from './view/point-view';
 import OfferFormView from './view/offer-form-view';
 import PointListView from './view/point-list-view';
+import MessageWithoutPoints from './view/empty-points-list';
 
 import {render, renderPosition} from './render.js';
 import { generatePoint } from './mock/point';
 
 
-const POINT_COUNT = 5;
+const POINT_COUNT = 0;
 const points = Array.from({length: POINT_COUNT}, generatePoint);
 
 const tripBody = document.querySelector('.page-body');
 const headerMenu = tripBody.querySelector('.trip-main');
 const siteMenuElement = tripBody.querySelector('.trip-controls__navigation');
 const filtersElement = tripBody.querySelector('.trip-controls__filters');
-const tripEventsElem = tripBody.querySelector('.trip-events');
+const mainContainer = tripBody.querySelector('.trip-events');
 
 const renderPoint = (pointListElement, point) => {
   const pointComponent = new PointView(point);
@@ -31,27 +32,43 @@ const renderPoint = (pointListElement, point) => {
     pointListElement.replaceChild(pointComponent.element, pointEditComponent.element);
   };
 
+  const onEscKeydowm = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeydowm);
+    }
+  };
+
   pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointToForm();
+    document.addEventListener('keydown', onEscKeydowm);
   });
 
   pointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeydowm);
   });
 
   render(pointListElement, pointComponent.element, renderPosition.BEFOREEND);
 };
 
 const pointListComponent = new PointListView();
-render(tripEventsElem, pointListComponent.element, renderPosition.BEFOREEND);
+render(mainContainer, pointListComponent.element, renderPosition.BEFOREEND);
 
-render(headerMenu, new HeaderInfoView(points[0]).element, renderPosition.AFTERBEGIN);
+
 render(siteMenuElement, new SiteMenuView().element, renderPosition.BEFOREEND);
 render(filtersElement, new FilterView().element, renderPosition.BEFOREEND);
-render(tripEventsElem, new SortView().element, renderPosition.AFTERBEGIN);
 
 
-for (let i = 0; i < POINT_COUNT; i++) {
-  renderPoint(pointListComponent.element, points[i]);
+if (points.length === 0) {
+  render(pointListComponent.element, new MessageWithoutPoints().element, renderPosition.BEFOREEND);
+} else {
+  render(headerMenu, new HeaderInfoView(points[0]).element, renderPosition.AFTERBEGIN);
+  render(mainContainer, new SortView().element, renderPosition.AFTERBEGIN);
+
+  for (let i = 0; i < POINT_COUNT; i++) {
+    renderPoint(pointListComponent.element, points[i]);
+  }
 }
